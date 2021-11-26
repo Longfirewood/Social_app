bdfcdf4cdfeb5a0b66dcf23eb686253d113117f3
Dolgodrov <m.dolgodrov@osome.com>
2021-11-26 22:21:22 +0300

    commit1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img width="100" src='https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = () => {
+    return <nav className='nav'>
+    <div>
+      Chat
+    </div>
+    <div>
+      Invoices
+    </div>
+    <div>
+      Payments
+    </div>
+    <div>
+      Billing
+    </div>
+    <div>
+      Settings
+    </div>
+  </nav>
+}
+
+export default Nav;
